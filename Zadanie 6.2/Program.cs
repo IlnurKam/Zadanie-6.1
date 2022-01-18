29ad77089f4128307b382db889420f4e90b3a63d
kamal673 <kamal673@yandex.ru>
2022-01-18 21:01:41 +0300

    Добавьте файлы проекта.

---
+++ Zadanie 6.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeWork6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string str = Console.ReadLine();
+            bool b = true;
+            str = str.Replace(" ", string.Empty);
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length - i - 1])
+                    b = false;
+            }
+
+            Console.WriteLine(((b) ? "Является" : "Не является") + " палиндромом");
+            Console.ReadKey();
+        }
+    }
+}
